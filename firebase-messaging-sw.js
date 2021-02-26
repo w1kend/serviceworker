28ceb55cb54899710fb27e6273b96311fe21a7a3
Sergey Makarov <wikendp@gmail.com>
2021-02-26 12:41:00 +0300

    1

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -16,32 +16,32 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 // Customize notification handler
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('Handling background message', payload);
-
-  // Copy data object to get parameters in the click handler
-  payload.notification = JSON.parse(JSON.stringify(payload.notification));
-
-  return self.registration.showNotification(payload.notification.title, payload.notification);
-});
-
-self.addEventListener('notificationclick', function(event) {
-  const target = event.notification.data.click_action || '/';
-  event.notification.close();
-
-  // This looks to see if the current is already open and focuses if it is
-  event.waitUntil(clients.matchAll({
-    type: 'window',
-    includeUncontrolled: true
-  }).then(function(clientList) {
-    // clientList always is empty?!
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url === target && 'focus' in client) {
-        return client.focus();
-      }
-    }
-
-    return clients.openWindow(target);
-  }));
-});
+// messaging.setBackgroundMessageHandler(function(payload) {
+//   console.log('Handling background message', payload);
+//
+//   // Copy data object to get parameters in the click handler
+//   payload.notification = JSON.parse(JSON.stringify(payload.notification));
+//
+//   return self.registration.showNotification(payload.notification.title, payload.notification);
+// });
+//
+// self.addEventListener('notificationclick', function(event) {
+//   const target = event.notification.data.click_action || '/';
+//   event.notification.close();
+//
+//   // This looks to see if the current is already open and focuses if it is
+//   event.waitUntil(clients.matchAll({
+//     type: 'window',
+//     includeUncontrolled: true
+//   }).then(function(clientList) {
+//     // clientList always is empty?!
+//     for (var i = 0; i < clientList.length; i++) {
+//       var client = clientList[i];
+//       if (client.url === target && 'focus' in client) {
+//         return client.focus();
+//       }
+//     }
+//
+//     return clients.openWindow(target);
+//   }));
+// });
